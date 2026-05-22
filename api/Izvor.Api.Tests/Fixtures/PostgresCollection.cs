c74338655f45fc80d4b8c5478278dd96f877f122
Jovan Vukojevic <35616305+JovanVukojevic@users.noreply.github.com>
2026-05-22 21:23:28 +0200

    Move tests to test branch

--- api/Izvor.Api.Tests/Fixtures/PostgresCollection.cs
+++
@@ -1,9 +0,0 @@
-using Xunit;
-
-namespace Izvor.Api.Tests.Fixtures;
-
-[CollectionDefinition(Name)]
-public sealed class PostgresCollection : ICollectionFixture<PostgresFixture>
-{
-    public const string Name = "Postgres";
-}
